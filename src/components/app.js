e4e2406f19394e9cec790fd3c0f6ba21fad3e027
Jonathan Reading <jon.reading.email@gmail.com>
2017-07-12 15:56:23 +0100

    Added async middleware

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import UserList from './user_list';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <h1>App</h1>
+        <UserList />
+      </div>
     );
   }
 }
---
+++ src/middleware/async.js
@@ -0,0 +1,12 @@
+export default function({dispatch}) {
+  return next => action => {
+    if(!action.payload || !action.payload.then) {
+      return next(action);
+    }
+    action.payload
+      .then(({data}) => {
+        const newAction = {...action, payload:data};
+        dispatch(newAction);
+      });
+  }
+}
